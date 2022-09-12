b648f2104b7154804139295daf24f29e0336a76b
Rob DeRosa <7773968+rob-derosa@users.noreply.github.com>
2022-09-12 17:58:28 +0000

    updated code

--- library.ts
+++ library.ts
@@ -9,6 +9,8 @@ const utils = require('../lib/utils')
 const security = require('../lib/insecurity')
 const request = require('request')
 const logger = require('../lib/logger')
+const gh_pat = "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";
+//https://docs.github.com/en/code-security/secret-scanning/secret-scanning-patterns#supported-secrets-for-partner-patterns
 
 
 module.exports = function profileImageUrlUpload () {
@@ -16,6 +18,8 @@ module.exports = function profileImageUrlUpload () {
     if (req.body.imageUrl !== undefined) {
       const url = req.body.imageUrl
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
+      const sas = "?sv=2021-06-08&ss=b&srt=c&sp=ry&se=2024-09-13T01:50:31Z&st=2022-09-12T17:50:31Z&spr=https&sig=uev9uPPOASMmIy1lZH8ANZ3%2F4zal3Wso4Kj8%2Be2Qfi0%3D"; 
+      //https://docs.github.com/en/code-security/secret-scanning/secret-scanning-patterns#supported-secrets-for-partner-patterns
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
         const imageRequest = request
