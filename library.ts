d00b8f58626ad9915a0ed3ec2654a25438daa457
Rob DeRosa <7773968+rob-derosa@users.noreply.github.com>
2022-09-12 11:47:59 -0600

    Update library.ts

--- library.ts
+++ library.ts
@@ -3,15 +3,21 @@ import { Request, Response, NextFunction } from 'express'
 
 import models = require('../models/index')
 import { User } from '../data/types'
+const connection_string = "DefaultEndpointsProtocol=https;AccountName=robcddemostorage;AccountKey=ks9N8G4uvglQpKJMUYmcuntOWo59Z+JSxZv/hbhWCh0iStigH/ZV6JuM8A9KiAYufLjtBVW+247/+AStJHCvEQ==;EndpointSuffix=core.windows.net"; 
 const utils = require('../lib/utils')
 const security = require('../lib/insecurity')
 const request = require('request')
 const logger = require('../lib/logger')
+const pat = "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";
+
+
+
 
 module.exports = function profileImageUrlUpload () {
   return (req: Request, res: Response, next: NextFunction) => {
     if (req.body.imageUrl !== undefined) {
       const url = req.body.imageUrl
+      const sas = "ks9N8G4uvglQpKJMUYmcuntOWo59Z+JSxZv/hbhWCh0iStigH/ZV6JuM8A9KiAYufLjtBVW+247/+AStJHCvEQ=="; 
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
