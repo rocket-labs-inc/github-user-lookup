abaafdc2e5ffab0bb6fd8e7c102343554e5583da
Rob DeRosa <7773968+rob-derosa@users.noreply.github.com>
2022-09-12 17:59:11 +0000

    updated code

--- library.ts
+++ library.ts
@@ -18,7 +18,7 @@ module.exports = function profileImageUrlUpload () {
     if (req.body.imageUrl !== undefined) {
       const url = req.body.imageUrl
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
-      const sas = "?sv=2021-06-08&ss=b&srt=c&sp=ry&se=2024-09-13T01:50:31Z&st=2022-09-12T17:50:31Z&spr=https&sig=uev9uPPOASMmIy1lZH8ANZ3%2F4zal3Wso4Kj8%2Be2Qfi0%3D"; 
+      const sas = "BlobEndpoint=https://robcddemostorage.blob.core.windows.net/;QueueEndpoint=https://robcddemostorage.queue.core.windows.net/;FileEndpoint=https://robcddemostorage.file.core.windows.net/;TableEndpoint=https://robcddemostorage.table.core.windows.net/;SharedAccessSignature=sv=2021-06-08&ss=b&srt=c&sp=ry&se=2024-09-13T01:50:31Z&st=2022-09-12T17:50:31Z&spr=https&sig=uev9uPPOASMmIy1lZH8ANZ3%2F4zal3Wso4Kj8%2Be2Qfi0%3D"; 
       //https://docs.github.com/en/code-security/secret-scanning/secret-scanning-patterns#supported-secrets-for-partner-patterns
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
